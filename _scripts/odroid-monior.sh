7eb0992c839f2766ad277066185649b4543a3cc6
odroid-br <rodolfotiago@linuxmail.org>
2016-07-08 11:27:11 -0300

    Update odroid-monior.sh
    usar apenas o _posts, ou seja tirando o "includes" da jogada. Assim faz com que o post aparece corretamente nos feeds rss

--- _scripts/odroid-monior.sh
+++ _scripts/odroid-monior.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# set -e --- em qualquer erro que houver, script vai terminar
+# set -e 
+
+git pull
+
 name="$(date +%Y-%m-%d)-pilar.markdown"
 
 cd /home/odroid/odroid-br.github.io/_posts/
 
-echo "<html>" > ${name}
-echo -e "uptime excutado em $(date)" >> ${name}
-echo "<br>" >> ${name}
-uptime >> ${name}
-echo "<br>" >> ${name}
-lscpu >> ${name}
-echo "<br>" >> ${name}
-echo "ha $(who | grep 192 | wc -l) dispositivos conectado(s) em mim ;)" >> ${name}
-echo "</html>" >> ${name}
+echo -e "
+---
+layout: post
+title:  "Pilar Monitoramento Odroid "
+categories: odroid pilar
+---" > ${name}
+echo "" >> ${name}
+echo -e "
+Post-script excutado em - $(date)
+$(uptime)
+$(df -h)
+ha $(who | grep 192 | wc -l) dispositivos conectado(s) em mim ;)
+" >> ${name}
 
 git add --all
 git commit -m "${name}"
