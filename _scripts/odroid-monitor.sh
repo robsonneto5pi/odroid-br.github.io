6cea16065e1a4392ec94820b892172f790c386a4
odroid-br <rodolfotiago@linuxmail.org>
2016-07-13 08:25:37 -0300

    Update odroid-monitor.sh

--- _scripts/odroid-monitor.sh
+++ _scripts/odroid-monitor.sh
@@ -35,7 +35,8 @@ layout: post
 title:  \"Monitoramento Meu Odroid\"
 ---" > ${name}
 echo "<br />" >> ${name}
-echo "Hello friends! Meu nome é $(whoami), mas podem me chamar de Mr. $(whoami | sed 's/o/0/g'). Abaixo você irá encontrar um relatório de monitoramento <br />" >> ${name}
+echo "Hello friends! Meu nome é $(whoami). Esse é uma publicação automática. <br />" ${name}
+echo "Abaixo você irá encontrar um relatório de monitoramento <br />" >> ${name}
 echo "Eu estou ligado ininterruptamente à $(uptime -p | cut -d' ' -f2-) !!!" >> ${name}
 echo "" >> ${name}
 echo "Script de monitoramento executado em - $(date) <br />"  >> ${name}
@@ -44,21 +45,24 @@ echo "Verificação de espaço em disco (comando usado: df -h)" >> ${name}
 echo "{% highlight ruby %}" >> ${name}
 echo "$(df -h)" >> ${name}
 echo "{% endhighlight %}" >> ${name}
+echo "" >> ${name}
 echo "Há $(who -a | grep 192 | wc -l) usuário(s) conectado(s) em mim ;) <br />" >> ${name}
 echo "Comando usado para ver quem está conectado: who -a" >> ${name}
 echo "{% highlight ruby %}" >> ${name}
 echo "$(who -a)" >> ${name}
 echo "{% endhighlight %}" >> ${name}
+echo "" >> ${name}
 echo "Informações sobre minha cpu (comando usado: lscpu)" >> ${name}
 echo "{% highlight ruby %}" >> ${name}
 echo "$(lscpu)" >> ${name}
 echo "{% endhighlight %}" >> ${name}
-echo "<br />" >> ${name}
+echo "" >> ${name}
 echo "Mais infomações sobre o meu sistema, lembrando que eu sou baseado em $(uname) <br />" >> ${name}
 echo "Comando usado: uname -a" >> ${name}
 echo "{% highlight ruby %}" >> ${name}
 echo "$(uname -a)" >> ${name}
 echo "{% endhighlight %}" >> ${name}
+echo "" >> ${name}
 #echo "Listando todos os programas instalados, Comando usado: dpkg -l" >> ${name}
 #echo "{% highlight ruby %}" >> ${name}
 #echo "$(dpkg -l)" >> ${name}
