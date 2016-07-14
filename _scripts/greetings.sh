63163b6e650d5ed1aa5c5d27eec69d7061b95674
odroid-br <rodolfotiago@linuxmail.org>
2016-07-14 17:30:59 -0300

    Update greetings.sh

--- _scripts/greetings.sh
+++ _scripts/greetings.sh
@@ -13,6 +13,7 @@ echo "2_Hello Friends! Meu nome é $(whoami)." >> greetings.txt
 echo "3_Hi There! Sometimes I´d prefer speaking in English, if you don´t mind!" >> greetings.txt
 echo "4_Hello! No momento, estou com $(ps aux | wc -l) processos em execução!" >> greetings.txt
 echo "5_Olá, eu fiz uma busca agora no ipinfo.io e descobri que minha região é: $(curl -s ipinfo.io | grep region | cut -d':' -f2 | sed 's/[\", ]//g')" >> greetings.txt
+echo "6_Hello Friends! No meu relógio agora são: $(date +%H:%M)." >> greetings.txt
 
 git add --all
 git commit -m "greetings uhu"
