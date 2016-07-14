2c8785934096e0345699d9572db43579f87a3930
odroid-br <rodolfotiago@linuxmail.org>
2016-07-14 16:21:03 -0300

    Update greetings.sh

--- _scripts/greetings.sh
+++ _scripts/greetings.sh
@@ -4,6 +4,10 @@
 
 cd /home/odroid/rodolfo/odroid-br.github.io/_scripts
 
+git reset --hard
+git clean -fd
+git up
+
 echo "1_Hello Friends! Esse é mais uma publicação automática. Cliquem [aqui](//odroid-br.github.io/../../../odroid-wiki) para saber mais sobre mim!" > greetings.txt
 echo "2_Hello Friends! Meu nome é $(whoami)." >> greetings.txt
 echo "3_Hi There! Sometimes I´d prefer speaking in English, if you don´t mind!" >> greetings.txt
