8ed87de7ee911c150d67b7f5de34515950422aba
odroid-br <rodolfotiago@linuxmail.org>
2016-07-14 14:50:08 -0300

    Update odroid-monitor.sh

--- _scripts/odroid-monitor.sh
+++ _scripts/odroid-monitor.sh
@@ -38,7 +38,7 @@ layout: post
 title:  \"Monitoramento Meu Odroid\"
 ---" > ${name}
 echo "<br />" >> ${name}
-echo "$(cat grettings.txt | grep ^5_ | cut -d'_' -f2-)  <br />" >> ${name}
+echo "$(cat ../_scripts/greetings.txt | grep ^5_ | cut -d'_' -f2-)  <br />" >> ${name}
 #echo "Hello friends! Meu nome é $(whoami). Esse é uma publicação automática. <br />" >> ${name}
 echo "Abaixo você irá encontrar um relatório de monitoramento <br />" >> ${name}
 echo "Eu estou ligado ininterruptamente à $(uptime -p | cut -d' ' -f2-) !!!" >> ${name}
