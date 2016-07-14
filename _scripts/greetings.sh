0f944914b04f7f257cdad191ef2d53cd641782b5
odroid-br <rodolfotiago@linuxmail.org>
2016-07-14 16:15:23 -0300

    Create greetings.sh

---
+++ _scripts/greetings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+### _monitorar_ ##
+
+cd /home/odroid/rodolfo/odroid-br.github.io/_scripts
+
+echo "1_Hello Friends! Esse é mais uma publicação automática. Cliquem [aqui](//odroid-br.github.io/../../../odroid-wiki) para saber mais sobre mim!" > greetings.txt
+echo "2_Hello Friends! Meu nome é $(whoami)." >> greetings.txt
+echo "3_Hi There! Sometimes I´d prefer speaking in English, if you don´t mind!" >> greetings.txt
+echo "4_Hello! No momento, estou com $(ps aux | wc -l) processos em execução!" >> greetings.txt
+echo "5_Olá, eu fiz uma busca agora no ipinfo.io e descobri que minha região é: $(curl -s ipinfo.io | grep region | cut -d':' -f2 | sed 's/[\", ]//g')" >> greetings.txt
