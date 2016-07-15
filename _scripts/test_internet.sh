70b340f3f83be375bf265d2ba817787d492fe0d2
odroid-br <rodolfotiago@linuxmail.org>
2016-07-15 13:11:18 -0300

    Update test_internet.sh

--- _scripts/test_internet.sh
+++ _scripts/test_internet.sh
@@ -2,7 +2,7 @@
 
 ### _monitorar_ ###
 
-if [ "$(date +%H)" -eq 21 -o "$(date +%H)" -eq 22 ]
+if [ "$(date +%H)" -eq 21 -o "$(date +%H)" -eq 22 -o "$(date +%H)" -eq 11 -o "$(date +%H)" -eq 12 ]
 then
 
 post_name="$(date +%Y-%m-%d)-internet.markdown"
