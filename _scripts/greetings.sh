ace3c2e9ac35c79845e598a22deebccd1ccc7507
odroid-br <rodolfotiago@linuxmail.org>
2016-07-21 20:32:03 -0300

    Update greetings.sh

--- _scripts/greetings.sh
+++ _scripts/greetings.sh
@@ -15,7 +15,7 @@ echo "4_Hello! No momento, estou com $(ps aux | wc -l) processos em execução!"
 echo "5_Olá, eu fiz uma busca agora no ipinfo.io e descobri que minha região é: $(curl -s ipinfo.io/region)" >> greetings.txt
 echo "6_Hello Friends! No meu relógio agora são: $(date +%H:%M)." >> greetings.txt
 echo "7_Oi! Vocês também podem acessar meu wordpress blog [aqui](https://tecnologiagourmet.wordpress.com/)." >> greetings.txt
-echo "8_Hello Friends! No roteador da minha rede local, há $(curl -s 192.168.1.1/padrao | grep -o -E -i IP:.{13} | sort -u | wc -l) dispositivo(s) conectado(s)" >> greetings.txt
+echo "8_Hello Friends! No roteador da minha rede local, há $(curl -s 192.168.1.1/padrao | grep -oP '(\d{1,3}\.){3}\d+' | grep ^192 | sort | uniq | wc -l) dispositivo(s) conectado(s)" >> greetings.txt
 echo "9_Olá, eu fiz uma busca agora no ipinfo.io e descobri que minha cidade é: $(curl -s ipinfo.io/city)" >> greetings.txt
 echo "10_Dolar agora: $(curl -s \"http://developers.agenciaideias.com.br/cotacoes/json\" | awk -F'\"' '{ print $16}')" >> greetings.txt
 
